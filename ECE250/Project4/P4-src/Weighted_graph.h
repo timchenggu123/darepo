8bcbdb7a349d214c7c6fd25eac3aa2a48dcb3115
Tim <2013tim.g@gmail.com>
2019-03-29 09:30:12 -0400

    pasted basic tests

--- ECE250/Project4/P4-src/Weighted_graph.h
+++ ECE250/Project4/P4-src/Weighted_graph.h
@@ -22,7 +22,7 @@ struct V
     double *mKey;
     int mIndex;
 
-    V(double key, int index) : mKey(&key), mIndex(index)
+    V(double *key, int index) : mKey(key), mIndex(index)
     {
     }
 
@@ -36,7 +36,6 @@ struct V
 class Custom_Priority_Queue{
 	private:
 		std::vector<V> mVertices;
-		int mSize;
 		void heapify(int);
 	public:
 		Custom_Priority_Queue();
@@ -46,49 +45,50 @@ class Custom_Priority_Queue{
 		void update();
 		bool empty();
 };
-Custom_Priority_Queue::Custom_Priority_Queue():
-	mSize(0){}
+Custom_Priority_Queue::Custom_Priority_Queue(){}
 
 void Custom_Priority_Queue::heapify(int index){
-	if (2*index > mSize -1){
+	if (2*index + 1 >= mVertices.size()){
 		return;
 	}
-	if (2*index + 1 > mSize -1){
-		if (*mVertices[2*index].mKey + 1 > *mVertices[index].mKey){
-			V temp = mVertices[index];
-			mVertices[index] = mVertices[2*index];
-			mVertices[2*index] = temp;
-		}
-		return;
-	}
-	if (*mVertices[index].mKey < *mVertices[2*index].mKey){
-		if (*mVertices[2*index].mKey < *mVertices[2*index + 1].mKey){
+	if (2*index + 2 >= mVertices.size()){
+		if (*mVertices[2*index + 1].mKey < *mVertices[index].mKey){
 			V temp = mVertices[index];
 			mVertices[index] = mVertices[2*index +1];
-			mVertices[2*index+ 1] = temp;
+			mVertices[2*index +1] = temp;
+		}
+		return;
+	}
+	if (*mVertices[index].mKey > *mVertices[2*index+1].mKey){
+		if (*mVertices[2*index+1].mKey > *mVertices[2*index + 2].mKey){
+			V temp = mVertices[index];
+			mVertices[index] = mVertices[2*index +2];
+			mVertices[2*index+ 2] = temp;
 		}else{
 			V temp = mVertices[index];
-			mVertices[index] = mVertices[2*index];
-			mVertices[2*index] = temp;
+			mVertices[index] = mVertices[2*index+1];
+			mVertices[2*index+1] = temp;
+
 		}
-	}else if (*mVertices[index].mKey < *mVertices[2*index + 1].mKey){
+	}else if (*mVertices[index].mKey > *mVertices[2*index + 2].mKey){
 		V temp = mVertices[index];
-		mVertices[index] = mVertices[2*index +1];
-		mVertices[2*index+ 1] = temp;
+		mVertices[index] = mVertices[2*index +2];
+		mVertices[2*index+ 2] = temp;
 	}
-	heapify(2*index);
-	heapify(2*index + 1);
+	heapify(2*index + 2);
+	heapify(2*index+1);
+
 }
 
 void Custom_Priority_Queue::push(V vertex){
 	mVertices.push_back(vertex);
-	mSize ++;
-	int i = mSize;
-	while (i/2 >= 0){
-		if (*mVertices[i].mKey > *mVertices[i/2].mKey){
+	int i = mVertices.size() -1;
+	while (i != 0){
+		if (*mVertices[i].mKey < *mVertices[(i-1)/2].mKey){
 			V temp = mVertices[i];
-			mVertices[i] = mVertices[i/2];
-			mVertices[i/2] = temp;
+			mVertices[i] = mVertices[(i-1)/2];
+			mVertices[(i-1)/2] = temp;
+			i = (i-1)/2;
 		}else{
 			break;
 		}
@@ -105,7 +105,6 @@ void Custom_Priority_Queue::pop(){
 	mVertices[mVertices.size() -1 ] = temp;
 	mVertices.pop_back();
 	heapify(0);
-	mSize --;
 }
 
 void Custom_Priority_Queue::update(){
@@ -113,7 +112,7 @@ void Custom_Priority_Queue::update(){
 }
 
 bool Custom_Priority_Queue::empty(){
-	if (mSize == 0){
+	if (mVertices.size() == 0){
 		return true;
 	}else{
 		return false;
@@ -123,8 +122,7 @@ bool Custom_Priority_Queue::empty(){
 class Weighted_graph {
 	private:
 		static const double INF;
-		double *weights;
-		double *keys;
+		double **weights;
 		int *v_degree;
 		int edgeCount;
 		int mSize;
@@ -150,67 +148,84 @@ class Weighted_graph {
 
 const double Weighted_graph::INF = std::numeric_limits<double>::infinity();
 
-Weighted_graph::Weighted_graph(int n = 50):
-weights(new double[n,n]),
-keys(new double[n]),
+Weighted_graph::Weighted_graph(int n):
+weights(new double*[n]),
 v_degree(new int[n]),
 edgeCount(0),
 mSize(n)
 {
 	//initializing the matrix to all infinity
 	for (int i = 0; i < n; i ++){
-		keys[i] = INF;
 		v_degree[i] = 0;
+		weights[i] = new double[n];
 		for (int j = 0; j< n; j++){
-			weights[i,j] = INF;
+			weights[i][j] = INF;
 		}
 	}
 
 }
-
+Weighted_graph::~Weighted_graph(){
+	for (int i =0; i<mSize; i++){
+		delete[] weights[i];
+		weights[i] = nullptr;
+	}
+	delete[] weights;
+	delete[] v_degree;
+}
 
 int Weighted_graph::degree (int n) const{
-	if (n > mSize-1 || n < 0){
+	if (n >= mSize || n < 0){
 		throw illegal_argument();
 	}
 	return v_degree[n];
 }
 
 int Weighted_graph::edge_count() const{
-
+	return edgeCount;
 }
 
 double Weighted_graph::adjacent( int m, int n ) const{
-	if (m > mSize-1 || n > mSize-1 || m <0 || n < 0){
+	if (m >= mSize || n >= mSize || m <0 || n < 0){
 		throw illegal_argument();
 	}
 	if(m == n){
 		return 0;
 	}
-	return weights[m,n];
-};
+	return weights[m][n];
+}
 
 double Weighted_graph::minimum_spanning_tree( int n) const{
-	if ( n > mSize-1 ||  n < 0){
+	if (edgeCount == 0){
+		return 0;
+	}
+	if ( n >= mSize ||  n < 0){
 		throw illegal_argument();
 	}
-	int MST_size = 0;
+	double MST_size = 0;
+	double *keys = new double[mSize];
+	for (int i = 0;i<mSize;i++){
+		keys[i] = INF;
+	}
 
 	Custom_Priority_Queue Q;
-	int T_member[mSize] = {};  //an array that holds whether a vertex is in Q. 0 is true 1 is false. 
-	Q.push(V(0,n));
+	int *T_member= new int[mSize];  //an array that holds whether a vertex is in Q. 0 is true 1 is false. ""
+	for (int i = 0; i < mSize; i++){
+		T_member[i] = 0;
+	}
+
 	keys[n] = 0;
+	Q.push(V(&keys[n],n));
 	while(!Q.empty()){
 		V u = Q.top();
-		Q.pop;
+		Q.pop();
 		T_member[u.mIndex] = 1; 
 		MST_size = MST_size + *u.mKey;
 		for(int v = 0; v< mSize; v++){
-			if (adjacent(v,u.mIndex) != INF){
-				if(!T_member[v] && adjacent(v,u.mIndex) < keys[v]){
+			if (!T_member[v] && adjacent(v,u.mIndex) != INF){
+				if(adjacent(v,u.mIndex) < keys[v]){
 					if (keys[v] == INF){
 						keys[v] = adjacent(v,u.mIndex);
-						Q.push(V(keys[v],v));
+						Q.push(V(&keys[v],v));
 					}else{
 						keys[v] = adjacent(v,u.mIndex);
 						Q.update();
@@ -219,64 +234,82 @@ double Weighted_graph::minimum_spanning_tree( int n) const{
 			}
 		}
 	}
-};
+
+	delete[] keys;
+	delete[] T_member;
+	return MST_size;
+}
 bool Weighted_graph::is_connected() const{
 	int n =0;
 	int nNodes = 0;
+	Custom_Priority_Queue Q;
+	int *T_member= new int[mSize];  //an array that holds whether a vertex is in Q. 0 is true 1 is false. ""
+	for (int i = 0; i < mSize; i++){
+		T_member[i] = 0;
+	};  //an array that holds whether a vertex is in Q. 0 is true 1 is false. ""
+	double *keys = new double[mSize];
+	for (int i = 0;i<mSize;i++){
+		keys[i] = INF;
+	}
 
-	std::priority_queue<V> Q;
-	int member[mSize] = {};  //an array that holds whether a vertex is in Q. 0 is true 1 is false. 
-	Q.push(V(0,n));
 	keys[n] = 0;
+	Q.push(V(&keys[n],n));
 	while(!Q.empty()){
 		V u = Q.top();
-		Q.pop;
-		member[u.mIndex] = 1; 
+		Q.pop();
+		T_member[u.mIndex] = 1;
 		nNodes ++;
 		for(int v = 0; v< mSize; v++){
 			if (adjacent(v,u.mIndex) != INF){
-				if(!member[v] && adjacent(v,u.mIndex) < keys[v]){
-					keys[v] = adjacent(v,u.mIndex);
-					Q.push(V(adjacent(v,u.mIndex),v));
+				if(!T_member[v] && adjacent(v,u.mIndex) < keys[v]){
+					if (keys[v] == INF){
+						keys[v] = adjacent(v,u.mIndex);
+						Q.push(V(&keys[v],v));
+					}else{
+						keys[v] = adjacent(v,u.mIndex);
+						Q.update();
+					}
 				}
 			}
-		
 		}
 	}
+	delete[] keys;
+	delete[] T_member;
 	if (nNodes == mSize){
 		return true;
 	}else{
 		return false;
 	}
-};
+}
 
 void Weighted_graph::insert( int m, int n, double w ){
-	if (m > mSize-1 || n > mSize-1 || m <0 || n < 0){
+	if (m >= mSize || n >= mSize || m <0 || n < 0){
 		throw illegal_argument();
 	}
 	if (w < 0 || w == INF){
 		throw illegal_argument();
 	}
 	if (w == 0){
-		if (weights[m,n] != INF){
+		if (weights[m][n] != INF){
 			v_degree[n] --;
 			v_degree[m] --;
 			edgeCount --;
 		}
-		weights[m,n] = INF;
-		weights[n,m] = INF;
+		weights[m][n] = INF;
+		weights[n][m] = INF;
 		return;
 	}
-	if (weights[m,n] == INF){
+	if (weights[m][n] == INF){
 		v_degree[n] ++;
 		v_degree[m] ++;
 		edgeCount ++;
 	}
-	weights[m,n] = w;
-	weights[n,m] = w;
+	weights[m][n] = w;
+	weights[n][m] = w;
+
 	//degree ++
 	//set edges for mn & nm.
-};
+}
 // Enter definitions for all public functions here
 
 std::ostream &operator << ( std::ostream &out, Weighted_graph const &graph ) {
